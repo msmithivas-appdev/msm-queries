b4c5d7b6d1de829389b25d1552b53729545f0e78
msmithivas <msmithivas@gmail.com>
2023-02-27 21:42:27 +0000

    Next commit Has partial director page built, and all directors page tables.

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,24 @@
 class DirectorsController < ApplicationController
 
   def index
+
+    @list_of_directors = Director.all
+
     render({ :template => "directors_template/index.html.erb" })
   end
+
+def profile
+
+@dir_profile = params.("profile")
+@dir_info = Director.where({ :id => @dir_profile.to_sym }).at(0)
+@dir_first_name = @dir_info.first_name
+
+
+render({ :template => "directors_template/dir_profile.html.erb" })
+
+end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
 
   get("/actors", { :controller => "actors", :action => "index" })
 
+ # get("/directors/youngest", { :controller => "", :action => "index" })
+
+ get("/directors/eldest", { :controller => "", :action => "index" })
+
+ get("/directors/:profile", { :controller => "directors", :action => "profile" })
   
 end
