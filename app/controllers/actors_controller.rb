b741d578732876379b1eef12aeb52d94e1b2c6b9
msmithivas <msmithivas@gmail.com>
2023-03-01 22:31:46 +0000

    Working on Filmography

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,17 +1,24 @@
 class ActorsController < ApplicationController
 
     $list_of_actors = Actor.all
-    @list_of_characters = Character.all
-    @list_of_movies = Movie.all
+    $list_of_characters = Character.all
+    $list_of_movies = Movie.all
+    $list_of_directors = Director.all
     
 
   def profile
 
   @actor_id = params.fetch("profile")
-  @character_hash = Character.where({ :id => @actor_id})
   
-  # @dir_id = @list_of_directors.where({ :id => @movie_id}).at(0)
-  # @dir_name = @dir_id.fetch("name")
+  
+  @actor_hash = $list_of_actors.where({ :id => @actor_id}).at(0)
+  @actor_name = @actor_hash.fetch("name")
+  @actor_dob = @actor_hash.fetch("dob")
+  @actor_bio = @actor_hash.fetch("bio")
+  @actor_image = @actor_hash.fetch("image")  
+
+
+  render({ :template => "actors_template/actor_profile.html.erb" })
 
   end
 
